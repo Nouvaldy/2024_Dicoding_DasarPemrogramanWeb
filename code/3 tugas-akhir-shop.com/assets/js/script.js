1b431b40a9694f5db6e89296bcb57cb9564a7747
Aldy <naufaldybambang.w@gmail.com>
2024-12-06 09:52:03 +0700

    Humberger menu + loading lazy img + masih da bug saat beralih dari viewport kecil ke besar

--- code/3 tugas-akhir-shop.com/assets/js/script.js
+++ code/3 tugas-akhir-shop.com/assets/js/script.js
@@ -14,3 +14,14 @@ window.onclick = function(event) {
       }
     }
 }
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myBurger() {
+  var x = document.querySelector(".nav-ul");
+  if (x.style.display == "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
